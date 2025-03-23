2bca088718c6455edcf9d0899b570111025c15e7
Krishaan Bhagat <knbhagat@wisc.edu>
2025-03-23 12:40:10 -0500

    corrected order of live bracket games

--- src/frontend-mm_app/src/Pages/Bracket/LiveBracketPage.tsx
+++ src/frontend-mm_app/src/Pages/Bracket/LiveBracketPage.tsx
@@ -10,6 +10,7 @@ export default function LiveBracketPage() {
 
     // formats the data to make it consistent with our bracket structure
     function formatDataIntoBracketStructure(data : any): any {
+        console.log("DATA", data);
         const roundObjArray: Round[] = []
         data.rounds.forEach((round : any, idx1 : any) => {
         // Skip the First Four
@@ -17,32 +18,44 @@ export default function LiveBracketPage() {
             // is in bracketed information
             const seedArray : Seed[] = [];
             if (idx1 < 5) {
-            round.bracketed.forEach((region: any, idx2: any) => {
-                region.games.forEach((game : any, idx3: any) => {
-                // grabs team info
-                const awayTeam : Team = {name: game.away.alias };
-                const homeTeam : Team = {name: game.home.alias };
-                // puts team info into an array
-                const teamArray : Team[] = [homeTeam, awayTeam];
-                // creates info abouth the game
-                const teamInfo : Seed = {id: game.id, teams: teamArray, date: new Date(game.scheduled).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" }), region: region.bracket.name.match(/^(\w+)/)?.[1].toUpperCase() || ""}
-                // pushes all seed info into an array from a selected round
-                seedArray.push(teamInfo)
+                const regionOrder = ["EAST", "MIDWEST", "SOUTH", "WEST",];
+                // Sort regions based on the predefined order
+                round.bracketed.sort((a: any, b: any) => {
+                    return regionOrder.indexOf(a.bracket.name.match(/^(\w+)/)?.[1].toUpperCase()) - regionOrder.indexOf(b.bracket.name.match(/^(\w+)/)?.[1].toUpperCase());
+                });
+                round.bracketed.forEach((region: any, idx2: any) => {
+                    // sort games
+                    region.games.sort((a: any, b: any) => {
+                        const gameNumberA = parseInt(a.title.match(/Game (\d+)/)?.[1]);
+                        const gameNumberB = parseInt(b.title.match(/Game (\d+)/)?.[1]);
+                        return gameNumberA - gameNumberB;
+                    });
+                    console.log(region.games)
+                    region.games.forEach((game : any, idx3: any) => {
+                    // grabs team info
+                    const awayTeam : Team = {name: game.away.alias };
+                    const homeTeam : Team = {name: game.home.alias };
+                    // puts team info into an array
+                    const teamArray : Team[] = [homeTeam, awayTeam];
+                    // creates info abouth the game
+                    const teamInfo : Seed = {id: game.id, teams: teamArray, date: new Date(game.scheduled).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" }), region: region.bracket.name.match(/^(\w+)/)?.[1].toUpperCase() || ""}
+                    // pushes all seed info into an array from a selected round
+                    seedArray.push(teamInfo)
+                    })
                 })
-            })
             // is in game information after the final 4
             } else { 
-            round.games.forEach((game : any) => {
-                // grabs team info
-                const awayTeam : Team = {name: game.away.alias };
-                const homeTeam : Team = {name: game.home.alias };
-                // puts team info into an array
-                const teamArray : Team[] = [homeTeam, awayTeam];
-                // creates info abouth the game
-                const teamInfo : Seed = {id: game.id, teams: teamArray, date: new Date(game.scheduled).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" })}
-                // pushes all seed info into an array from a selected round
-                seedArray.push(teamInfo)
-            })
+                round.games.forEach((game : any) => {
+                    // grabs team info
+                    const awayTeam : Team = {name: game.away.alias };
+                    const homeTeam : Team = {name: game.home.alias };
+                    // puts team info into an array
+                    const teamArray : Team[] = [homeTeam, awayTeam];
+                    // creates info abouth the game
+                    const teamInfo : Seed = {id: game.id, teams: teamArray, region: 'FINAL FOUR', date: new Date(game.scheduled).toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" })}
+                    // pushes all seed info into an array from a selected round
+                    seedArray.push(teamInfo)
+                })
             }
             // puts all seeds from a certain round into a round object then pushes it into an array to store all rounds
             const roundObj : Round = {title: round.name , seeds : seedArray};
